9fed38eb451ffa016fe2ccea1efe19c2f4c4736a
singingXY <singeve@qq.com>
2019-10-10 15:31:39 +0800

    list显示

--- src/views/List.vue
+++ src/views/List.vue
@@ -11,10 +11,12 @@
           v-for="dailyList in daily.list"
           :key="dailyList.id"
         >
-          <span :class="'type' + dailyList.type">{{
-            dailyList.type
-          }}</span>
-          {{ dailyList.date }}{{ dailyList.address }}
+          <span
+            class="daily-icon"
+            :class="'type' + dailyList.type"
+          ></span>
+          <span> {{ dailyList.date | dailyDate }}</span>
+          {{ dailyList.address }}
         </p>
         {{ daily.content }}
       </van-collapse-item>
@@ -46,7 +48,12 @@ export default {
       }
     )
   },
-  methods: {}
+  filters: {
+    dailyDate(e) {
+      var date = new Date(e)
+      return date.getHours() + ':' + date.getMinutes()
+    }
+  }
 }
 </script>
 <style>
@@ -56,8 +63,30 @@ export default {
 }
 .van-collapse .van-cell {
   font-size: 22px;
+  background: #f8f8f8;
+}
+.van-cell:not(:last-child)::after {
+  border-bottom: 1px solid #fff;
+}
+
+.van-hairline--top-bottom::after,
+.van-hairline-unset--top-bottom::after {
+  border-color: #fff;
 }
 .van-collapse p {
   font-size: 20px;
+}
+.daily-icon {
+  display: inline-block;
+  background-size: 100%;
+  width: 34px;
+  height: 34px;
+  vertical-align: text-bottom;
+}
+.daily-icon.type0 {
+  background: url(../assets/images/type0.png) center;
+}
+.daily-icon.type1 {
+  background: url(../assets/images/type1.png) center;
 }
 </style>
